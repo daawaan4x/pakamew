55312d6ef8b7613f429de592c60bbe283c0c30fe
Theone Eclarin <theoneeclarin@outlook.com>
2026-02-22 22:19:51 +0800

    fix: misc. package script & config changes
    - removed hardcoded host & port in vite server config
    - removed parallel dev command for server & web

--- server/vite.config.ts
+++ server/vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import { VitePluginNode } from "vite-plugin-node";
 
 export default defineConfig({
-	server: {
-		host: "127.0.0.1",
-		port: 3000,
-	},
 	plugins: [
 		...VitePluginNode({
 			adapter: async ({ app, req, res, next }) => {
